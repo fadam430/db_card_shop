966c5cd07b7957ee1b86015815103fc1fed1a744
fadam430 <f.adam430@gmail.com>
2026-04-22 17:02:26 +0100

    add base allauth template

--- db_cardshop/settings.py
+++ db_cardshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=y4uhk9d1szuz-!@w#%z64+uzw=r^&n$!co869i0z15z#!)hq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
