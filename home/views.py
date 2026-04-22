636b5465e3c1186ba6a4b40dd31008f992d99779
fadam430 <f.adam430@gmail.com>
2026-04-22 17:18:09 +0100

    add home app and templates

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    
+    return render(request, 'home/index.html')
