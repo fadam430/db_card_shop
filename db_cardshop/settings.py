9904413412528324b7fab14fed2da262699b957a
fadam430 <f.adam430@gmail.com>
2026-04-23 13:41:16 +0100

    add first card images

--- db_cardshop/settings.py
+++ db_cardshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=y4uhk9d1szuz-!@w#%z64+uzw=r^&n$!co869i0z15z#!)hq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -145,4 +145,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
