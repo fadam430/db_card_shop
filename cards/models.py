af6a29a38709a33042c058304d01e46874945dbc
fadam430 <f.adam430@gmail.com>
2026-04-24 11:31:59 +0100

    cards images update

--- cards/models.py
+++ cards/models.py
@@ -14,7 +14,7 @@ class Card(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image_url = models.URLField()
+    image = models.ImageField(upload_to='cards/', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- db_cardshop/settings.py
+++ db_cardshop/settings.py
@@ -148,3 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media files (User-uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
