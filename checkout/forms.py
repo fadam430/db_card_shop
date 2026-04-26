2ce77a66cf4a6345dc33fa5168213a7d18617454
fadam430 <f.adam430@gmail.com>
2026-04-26 17:26:03 +0100

    add forms and signal

---
+++ checkout/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['full_name', 'email', 'address_line1', 'address_line2', 'city', 'postal_code', 'country']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholder_texts = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'address_line1': 'Street Address 1',
+            'address_line2': 'Street Address 2 (optional)',
+            'city': 'City',
+            'postal_code': 'Postal Code',
+            'country': 'Country',
+        }
+        self.fields['full_name'].widget.attrs['auto-focus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f"{placeholder_texts[field]} *"
+            else:
+                placeholder = placeholder_texts[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
