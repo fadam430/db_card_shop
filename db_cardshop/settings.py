1cc7b2b3ded6a2e4886ee20c2476ce9af5a7622f
fadam430 <f.adam430@gmail.com>
2026-04-27 12:15:55 +0100

    deploy to heroku

--- db_cardshop/settings.py
+++ db_cardshop/settings.py
@@ -31,7 +31,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['db-card-shop-5ba6aba51d41.herokuapp.com/', 'localhost', '127.0.0.1']
 
