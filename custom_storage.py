f37767cef7ab1918e88f40c28d152908f53e2a9a
fadam430 <f.adam430@gmail.com>
2026-07-01 11:52:29 +0100

    costume storage changes

--- custom_storage.py
+++ custom_storage.py
@@ -5,5 +5,5 @@ class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = settings.MEDIA_FILES_LOCATION
     
