9ed0451e89d0495b46d7ceb166727baab4d6d7c9
fadam430 <f.adam430@gmail.com>
2026-09-02 21:58:29 +0100

    card page change

--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Card, Category
+from .models import Card
 
 # Create your views here.
 
 def card_list(request):
     """Display all cards in a mobile-first grid"""
     cards = Card.objects.all()
-    return render(request, 'cards/card_list.html', {'cards': cards, 'active_page': 'cards'})
+    return render(request, 'templates/cards/card_list.html', {'cards': cards, 'active_page': 'cards'})
